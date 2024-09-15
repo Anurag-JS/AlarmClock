34f4cf113dda098137230103a53699522a855d61
Anurag <anuragv8269@gmail.com>
2024-09-16 00:11:16 +0530

    Fixed time format issue

--- src/Alarm.js
+++ src/Alarm.js
@@ -2,13 +2,12 @@ import { useEffect, useState } from "react"
 
 export default function Alarm (){
 
-    const [time, setTime] = useState(new Date().getTime());
-    const [alarmTime, setAlarmTime] = useState();
+    const [time, setTime] = useState(new Date());
+    const [alarmTime, setAlarmTime] = useState("");
 
     const checkAlarm = () => {
-        const currentTime = String(time).slice(0,5);
-        console.log(currentTime);
-        if(time===alarmTime){
+        const currentTime = time.toTimeString().slice(0,5);
+        if(currentTime===alarmTime){
             return(
                 <div>
                     <h1>Hey, Your Alarm is On</h1>
@@ -18,13 +17,31 @@ export default function Alarm (){
         }
     }
 
+    const handleInputChange= (e)=>{
+        setAlarmTime(e.target.value)
+    }
+
+ 
+    
     useEffect(()=>{
         const timerId = setInterval(()=>{
-            setTime(new Date().getTime());
+            setTime(new Date());
         },1000)
 
         return () => clearInterval(timerId);
     },[])
+
+    const demo = () => {
+        // setTime(time.toLocaleString("en-In",{
+        //     hour: "2-digit",
+        //     minute : "2-digit"
+        // }))
+        setAlarmTime(alarmTime)
+        const input = alarmTime;
+        const curr = time;
+        console.log(`input = ${input}, ${typeof input}|||  current ${curr}, ${typeof curr}`);
+
+    }
 
 
     
@@ -32,10 +49,10 @@ export default function Alarm (){
     return(
         <div>
             <h2>Set up Alarm</h2>
-            <input type="time" value={alarmTime} onChange={(e)=> {setAlarmTime(e.target.value); checkAlarm()}}/>
-            {checkAlarm}
-            <button >Save Alarm</button>
-            {console.log(alarmTime, time)}
+            <input type="time" value={alarmTime} onChange={handleInputChange} /> 
+            {checkAlarm()}
+            <button onClick={demo}>Save Alarm</button>
+            
         </div> 
     )
 }
